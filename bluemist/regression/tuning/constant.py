961832a2f430f6b9e34555a2b2b247450b7e8f55
Shashank Agrawal <shashanka89@gmail.com>
2023-02-13 00:14:16 -0800

    Added more hyperparameters

--- bluemist/regression/tuning/constant.py
+++ bluemist/regression/tuning/constant.py
@@ -35,11 +35,12 @@ default_hyperparameters = {
     'learning_rate': np.arange(0.1, 50 + 1, 0.5).tolist(),
     'max_bins': np.arange(2, 255 + 1, 1).tolist(),
     'max_depth': [None, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    'max_features': [0.7, 0.8, 0.9, 1],
     'max_iter': np.arange(100, 5000 + 1, 50).tolist(),
     'max_leaf_nodes': np.arange(2, 50 + 1, 1).tolist(),
     'max_n_alphas': np.arange(100, 5000 + 1, 100).tolist(),
     'max_samples': [0.7, 0.8, 0.9, 1],
-    'max_features': [0.7, 0.8, 0.9, 1],
+    'max_subpopulation': [1e6, 1e5, 10000, 1000, 100, 1],
     'metric': ['minkowski'],
     'metric_params': [None],
     'min_impurity_decrease': [0.0],
@@ -52,9 +53,10 @@ default_hyperparameters = {
     'n_iter': np.arange(100, 5000 + 1, 50).tolist(),
     'n_iter_no_change': [None, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     'n_jobs': [-1],
+    'n_neighbors': np.arange(1, 10 + 1, 1).tolist(),
+    'n_subsamples': [None, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     'normalize': [False],
     'normalize_y': [True, False],
-    'n_neighbors': np.arange(1, 10 + 1, 1).tolist(),
     'oob_score': [True, False],
     'p': [1, 2, 3, 4, 5],
     'positive': [True, False],
@@ -204,3 +206,97 @@ OrthogonalMatchingPursuitCV = {
     'max_iter': [5]
 }
 
+PLSCanonical = {
+    'algorithm': ['nipals', 'svd']
+}
+
+PassiveAggressiveRegressor = {
+    'C': [1.0],
+    'n_iter_no_change': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    'loss': ['epsilon_insensitive', 'squared_epsilon_insensitive'],
+    'epsilon': [1e-8, 1e-7, 1e-06, 1e-05, 0.0001, 0.001, 0.01, 0.1],
+    'average': [True, False]
+}
+
+QuantileRegressor = {
+    'solver': ['highs-ds', 'highs-ipm', 'highs', 'interior-point', 'revised simplex'],
+    'solver_options': [None],
+    'quantile': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+}
+
+RANSACRegressor = {
+    'min_samples': [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+    'residual_threshold': [None],
+    'is_data_valid': [None],
+    'is_model_valid': [None],
+    'max_trials': np.arange(100, 1000 + 1, 50).tolist(),
+    'max_skips': [np.inf],
+    'stop_n_inliers': [np.inf],
+    'stop_score': [np.inf],
+    'stop_probability': [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99, 1.0],
+    'loss': ['absolute_error', 'squared_error']
+}
+
+RadiusNeighborsRegressor = {
+    'radius': [1.0],
+    'weights': ['uniform', 'distance'],
+    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+    'leaf_size': np.arange(2, 50 + 1, 1).tolist(),
+    'p': [1, 2],
+    'metric': ['euclidean', 'minkowski'],
+    'metric_params': [None]
+}
+
+RandomForestRegressor = {
+    'max_features': [None, 'sqrt', 'log2', 0.7, 0.8, 0.9, 1]
+}
+
+Ridge = {
+    'solver': ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga', 'lbfgs']
+}
+
+RidgeCV = {
+    'alphas': [(0.1, 1.0, 10.0)],
+    'scoring': [None],
+    'gcv_mode': ['auto', 'svd', 'eigen'],
+    'store_cv_values': [False],
+    'alpha_per_target': [False],
+    'cv': [2, 3, 4, 5, 6, 7, 8, 9, 10],
+}
+
+SGDRegressor = {
+    'loss': ['squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive'],
+    'penalty': ['l2', 'l1', 'elasticnet', None],
+    'alpha': [1e-06, 1e-05, 0.0001, 0.001, 0.01, 0.1],
+    'l1_ratio': [0.15],
+    'epsilon': [1e-8, 1e-7, 1e-06, 1e-05, 0.0001, 0.001, 0.01, 0.1],
+    'learning_rate': ['constant', 'optimal', 'invscaling', 'adaptive'],
+    'eta0': [0.01],
+    'power_t': [0.25],
+    'validation_fraction': [0.1],
+    'average': [True, False]
+}
+
+SVR = {
+    'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+    'gamma': ['scale', 'auto'],
+    'coef0': [0.0],
+    'C': [1.0],
+    'epsilon': [1e-8, 1e-7, 1e-06, 1e-05, 0.0001, 0.001, 0.01, 0.1],
+    'shrinking': [True, False],
+    'cache_size': [200]
+}
+
+TransformedTargetRegressor = {
+    'regressor': [None],
+    'transformer': [None],
+    'func': [None],
+    'inverse_func': [None],
+    'check_inverse': [True, False]
+}
+
+TweedieRegressor = {
+    'power': [0, 1, 2, 3],
+    'alpha': [0.0, 1.0, 1.5, 2.0, 2.5, 3, 3.5, 4, 4.5, 5],
+    'link': ['auto', 'identity', 'log']
+}
