1df43c71c382d08803b88d59248df7f45298faaa
Shashank Agrawal <shashanka89@gmail.com>
2023-06-11 03:57:13 -0700

    Constant file to store common constants for use across the application

---
+++ bluemist/utils/constants.py
@@ -0,0 +1,13 @@
+
+# Author: Shashank Agrawal
+# License: MIT
+# Version: 0.1.2
+# Email: dew@bluemist-ai.one
+# Created: Jun 11, 2023
+# Last modified: Jun 11, 2023
+
+GPU_BRAND_INTEL = 'Intel'
+GPU_BRAND_NVIDIA = 'NVIDIA'
+
+GPU_EXECUTION_DEVICE_INTEL = 'Intel GPU'
+GPU_EXECUTION_DEVICE_NVIDIA = 'NVIDIA GPU'
