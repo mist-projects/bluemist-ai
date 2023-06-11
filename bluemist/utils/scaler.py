87932bbe89c30eb592cb41b514e2f71844fcf60b
Shashank Agrawal <shashanka89@gmail.com>
2023-06-11 03:59:47 -0700

    Restructured file for better readability

--- bluemist/utils/scaler.py
+++ bluemist/utils/scaler.py
@@ -1,18 +1,20 @@
+
+# Author: Shashank Agrawal
+# License: MIT
+# Version: 0.1.2
+# Email: dew@bluemist-ai.one
+# Created: Jun 22, 2022
+# Last modified: June 11, 2023
+
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler, MinMaxScaler, RobustScaler
 
-available_scalers = ['StandardScaler', 'MinMaxScaler', 'MaxAbsScaler', 'RobustScaler']
+available_scalers = {
+    'StandardScaler': StandardScaler,
+    'MinMaxScaler': MinMaxScaler,
+    'MaxAbsScaler': MaxAbsScaler,
+    'RobustScaler': RobustScaler
+}
 
 
-def getScaler(scaling_strategy):
-    scaler = None
-
-    if scaling_strategy == 'MaxAbsScaler':
-        scaler = MaxAbsScaler()
-    elif scaling_strategy == 'MinMaxScaler':
-        scaler = MinMaxScaler()
-    elif scaling_strategy == 'RobustScaler':
-        scaler = RobustScaler()
-    elif scaling_strategy == 'StandardScaler':
-        scaler = StandardScaler()
-
-    return scaler
+def get_scaler(scaling_strategy):
+    return available_scalers.get(scaling_strategy, None)()
